54599e6f7550bb9a8e23cbd252e182ba112bf167
podrion <pierre.odrion@orange.fr>
2022-03-16 16:00:41 +0100

    print

--- turtle-ast.c
+++ turtle-ast.c
@@ -425,6 +425,88 @@ void context_create(struct context *self) {
  * @param ctx the context
  */
 void ast_eval(const struct ast *self, struct context *ctx) {
+    if (!self) {
+        return;
+    }
+
+    ast_node_eval(self->unit, ctx);
+}
+
+void ast_node_eval(const struct ast_node *self, struct context *ctx) {
+    if (!self) {
+        return;
+    }
+
+
+}
+
+void eval_cmd_forward(const struct ast_node *self, struct context *ctx) {
+
+}
+void eval_cmd_backward(const struct ast_node *self, struct context *ctx) {
+
+}
+void eval_cmd_position(const struct ast_node *self, struct context *ctx) {
+
+}
+void eval_cmd_right(const struct ast_node *self, struct context *ctx) {
+
+}
+void eval_cmd_left(const struct ast_node *self, struct context *ctx) {
+
+}
+void eval_cmd_heading(const struct ast_node *self, struct context *ctx) {
+
+}
+void eval_cmd_up(const struct ast_node *self, struct context *ctx) {
+
+}
+void eval_cmd_down(const struct ast_node *self, struct context *ctx) {
+
+}
+void eval_cmd_print(const struct ast_node *self, struct context *ctx) {
+
+}
+void eval_cmd_color(const struct ast_node *self, struct context *ctx) {
+
+}
+void eval_cmd_color_yy(const struct ast_node *self, struct context *ctx) {
+
+}
+void eval_cmd_home(const struct ast_node *self, struct context *ctx) {
+
+}
+void eval_cmd_repeat(const struct ast_node *self, struct context *ctx) {
+
+}
+void eval_cmd_set(const struct ast_node *self, struct context *ctx) {
+
+}
+void eval_cmd_proc(const struct ast_node *self, struct context *ctx) {
+
+}
+void eval_cmd_call(const struct ast_node *self, struct context *ctx) {
+
+}
+void eval_func_sin(const struct ast_node *self, struct context *ctx) {
+
+}
+void eval_func_cos(const struct ast_node *self, struct context *ctx) {
+
+}
+void eval_func_tan(const struct ast_node *self, struct context *ctx) {
+
+}
+void eval_func_random(const struct ast_node *self, struct context *ctx) {
+
+}
+void eval_func_sqrt(const struct ast_node *self, struct context *ctx) {
+
+}
+void eval_binary_operand(const struct ast_node *self, struct context *ctx) {
+
+}
+void eval_unary_operand(const struct ast_node *self, struct context *ctx) {
 
 }
 
